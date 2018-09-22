ef24e5bd233d6ac8bc629059fa605911f0922fb2
Husam ABBOUD <github@drhus.com>
2018-09-22 13:31:06 -0300

    update

--- labx/bitcoin.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const bs58check = require('bs58check');
-
-// function is global to be accessible in main script
-window.encodeBitcoinAddress = function(address) {
- return bs58check.decode(address).toString('hex').slice(2);
-}
