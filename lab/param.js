e3611693aa30a512627eaf9a447c3f2795481270
Husam ABBOUD <github@drhus.com>
2018-09-23 19:36:29 -0300

    update HEXLIST_URL_DECODE_COLOR_MAP
    updating HEXLIST_URL_DECODE_COLOR_MAP

--- lab/param.js
+++ lab/param.js
@@ -7,9 +7,10 @@ const HEXLIST_NAMES = [
   'Momcode-210_Hus', 
   'Momcode-211_Hus',
   'Momcode-212_Hus',
-  'Momcode-301_Hus (ASCII Optimized + Letter Frequency)',
+  'Momcode-301_Hus (ASCII + Frequency)',
   '4Fun - IdentiAddress I',
   '4Fun - IdentiAddress II',
+  '4Fun - IdentiAddress III',  
   '4Fun - Emoji-Random',
   'empty-set',  
 ];
@@ -38,14 +39,44 @@ const HEXLIST_URL_SYMBOL_DELIMITER = '\x00';
 
 const HEXLIST_URL_ENCODE_COLOR_MAP = {
   'gray': 'z',
-  'orange': 'a',
+  'red': 'r',
+  'orange': 'o',
+  'yellow': 'y',
+  'green': 'g',
+  'blue': 'b',
+  'purple': 'p',
+  'white': 'w',  
   'black': 'k',
+  'bg-red': '1',
+  'bg-orange': '2',
+  'bg-yellow': '3',
+  'bg-green': '4',
+  'bg-blue': '5',
+  'bg-purple': '6',
+  'bg-white': '7',  
+  'bg-black': '8',  
+  'bg-gray': '9', 
 };
 
 const HEXLIST_URL_DECODE_COLOR_MAP = {
   'z': 'gray',
-  'a': 'orange',
-  'k': 'black',
+  'r': 'red',
+  'o': 'orange',
+  'y': 'yellow',
+  'g': 'green',
+  'b': 'blue',
+  'p': 'purple',
+  'w': 'white',
+  'b': 'black',
+  '1': 'bg-red',
+  '2': 'bg-orange',
+  '3': 'bg-yellow',
+  '4': 'bg-green',
+  '5': 'bg-blue',
+  '6': 'bg-purple',
+  '7': 'bg-white',
+  '8': 'bg-black',
+  '9': 'bg-gray',
 };
 
 
@@ -53,10 +84,18 @@ const HEXLIST_URL_ENCODE_STYLE_MAP = {
   'special': 'p',
   'small': 'm',
   'bold': 'l',
+  'underline': 'u',
+  'strikethrough': 's',
+  'overline': 'q',  
+  'italic': 'i',  
 };
 
 const HEXLIST_URL_DECODE_STYLE_MAP = {
   'p': 'special',
   'm': 'small',
   'l': 'bold',
+  'u': 'underline',
+  's': 'strikethrough',
+  'q': 'overline',  
+  'i': 'italic',  
 };
