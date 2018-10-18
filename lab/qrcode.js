080f8ee0cca466655376ae81bfd61b854ba0a6fc
alexandrtovmach <alexandrtovmach@gmail.com>
2018-10-18 16:06:09 +0300

    added controllers for momcode graphic

--- lab/qrcode.js
+++ lab/qrcode.js
@@ -1,6 +1,39 @@
 const qrCodeEl = document.querySelector('#qrcode-square');
+const [ stretchSlider, bgOpacity, bgColor ] = document.querySelector('.injection-controls').getElementsByTagName("input");
+
 const qr = new QRCode(qrCodeEl, {
   width: 500,
   height: 500,
   correctLevel : QRCode.CorrectLevel.H
 });
+
+
+stretchSlider.onchange = event => {
+  momCodeSqareInjected.classList.remove("stretch-0", "stretch-1", "stretch-2");
+  momCodeSqareInjected.classList.add(`stretch-${event.target.value}`);
+}
+
+bgOpacity.onchange = event => {
+  const currentColor = momCodeSqareInjected.style.backgroundColor || 'rgba(255,255,255,0.7)';
+  momCodeSqareInjected.style.backgroundColor = currentColor.replace(/[\d\.]+\)/, `${event.target.value})`);
+}
+
+bgColor.onchange = event => {
+  const currentColor = momCodeSqareInjected.style.backgroundColor || 'rgba(255,255,255,0.7)';
+  const newColor = hexToRgba(event.target.value, currentColor.match(/[\d\.]+\)/));
+  momCodeSqareInjected.style.backgroundColor = newColor;
+}
+
+function hexToRgba(hexVal, a) {
+  hexVal = hexVal.replace('#', '');
+  if ( hexVal.length === 3 ) {
+    hexVal = hexVal[0] + hexVal[0] + hexVal[1] + hexVal[1] + hexVal[2] + hexVal[2];
+  };
+  const red = hexVal.substr(0,2),
+      green = hexVal.substr(2,2),
+      blue = hexVal.substr(4,2);
+  const red10 = parseInt(red,16),
+      green10 = parseInt(green,16),
+      blue10 = parseInt(blue,16);
+  return `rgba(${red10},${green10},${blue10},${a || '1)'}`;
+}
